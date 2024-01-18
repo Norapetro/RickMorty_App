e2b30b5a0e82a541341bdcba981bebe7b7579480
Nora <adrianaguirrepetro@gmail.com>
2024-01-17 19:12:04 -0500

    config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @Component({
@@ -9,7 +13,11 @@ import { NavbarComponent } from "./navbar/navbar.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavbarComponent]
+    imports: [CommonModule, RouterOutlet, NavbarComponent,
+      MatProgressSpinnerModule,
+      MatCardModule, 
+      MatButtonModule, 
+      HttpClientModule]
 })
 export class AppComponent {
   title = 'RickMorty_Api';
--- src/app/observador/observador.component.ts
+++ src/app/observador/observador.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
 import { RickService } from '../rick.service';
-import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 
@@ -12,11 +8,7 @@ import { CommonModule } from '@angular/common';
   selector: 'app-observador',
   standalone: true,
   imports: [
-    CommonModule,
-    MatProgressSpinnerModule,
-    MatCardModule, 
-    MatButtonModule, 
-    HttpClientModule],
+    CommonModule],
   providers: [RickService],
   templateUrl: './observador.component.html',
   styleUrl: './observador.component.css'
